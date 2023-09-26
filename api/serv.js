33a6d1df628ba6fc5deba0d8e1fd80a68441ab37
Jahel Nogales <jahel.nogales@espoch.edu.ec>
2023-09-25 21:39:20 -0500

    Repaso Api

--- api/serv.js
+++ api/serv.js
@@ -1,11 +1,15 @@
 const http = require('node:http')
 
-const server = http.createServer((req,res) => {
+const request = http.createServer((req,res) => {
     console.log('request received')
     res.end('Hola Mundo')
-})
+}) //callback funcion que se ejecuta
+ 
+const server = http.createServer(request)
 
+//escuchar un servidor en un puerto
 server.listen(0, ()=>{
     //console.log('server listening on port 3000')
     console.log(`server listening on port http://localhost:${server.address().port}`)
 })
+
