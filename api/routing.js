9c7dc53fdab0d78ef397e758aaf3bda455eaa60b
Jahel Nogales <jahel.nogales@espoch.edu.ec>
2023-09-26 11:53:56 -0500

    GET y POST

--- api/routing.js
+++ api/routing.js
@@ -13,22 +13,39 @@ const processRequest = (req,res) => {
                 default:
                     res.statusCode=404
                     res.setHeader('Content-Type','text/html; charset=utf-8')
-                    return res.end('404')
+                    return res.end('<h1>Not found<h1>')
             }
     
     case 'POST':
         switch(url){
-            case '/pokemon':
+            case '/pokemon':{
                 let body =''
+                //escuchar el evento data
+                req.on('data', chunk =>{
+                    body+=chunk.toString()
+                })
+
+                req.on('end',()=>{
+                    console.log('body', body)
+                    const data=JSON.parse(body)
+                    res.writeHead(201, {'Content-Type':'application/json; charset=utf-8'})
+                    data.timestamp=Date.now()
+                    res.end(JSON.stringify(data))
+                })
+                break
+            }
+
+            default:
+                res.statusCode=404
+                res.setHeader('Content-Type','text/html; charset=utf-8')
+                return res.end('<h1>404 Not found<h1>')
         }
     }
 }
 
-
-
 const server = http.createServer(processRequest)
-server.listen(desiredPort, ()=>{
+server.listen(1234, ()=>{
     //console.log('server listening on port 3000')
-    console.log(`server listening on port http://localhost:${desiredPort}`)
+    console.log(`server listening on port http://localhost:${1234}`)
 })
 
