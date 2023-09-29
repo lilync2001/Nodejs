83ed3e6384a85d65739192b8054a1fb8696734c4
Jahel Nogales <jahel.nogales@espoch.edu.ec>
2023-09-28 22:04:49 -0500

    cambios

--- apiRest/app.js
+++ apiRest/app.js
@@ -1,121 +1,12 @@
-const express = require('express')
-const movies = require('./movie.json')
-const crypto = require('node:crypto')
-const { validateMovie, validatePartialMovie } = require('./schemas/movie')
-const cors=require('cors')
+import express, { json } from 'express'
+import { moviesRouter } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
 
 const app = express()
 app.disable('x-powered-by')
-app.use(express.json())
-app.use(cors()) // es un middleware
-
-/* const ACCEPTED_ORIGINS = [
-    'http://localhost:8080',
-    'http://localhost:1234',
-    'https://movies.com',
-    'https://midu.dev'
-] */
-
-
-//todos los recursos que sean moovies se indentifican como /movies
-app.get('/movies', (req,res)=>{
-   /* const origin = req.header('origin')
-   if(ACCEPTED_ORIGINS.includes(origin)|| !origin){
-    res.header('Access-Control-Allow-Origin',origin)
-
-  } 
-    */
-    const{genre}= req.query
-    if(genre){
-    const filterMovies = movies.filter(
-        movie=> movie.genre.some(g=>g.toLowerCase()== genre.toLowerCase())
-    )
-    return res.json(filterMovies)
-    }
-    res.json(movies)
-})
-
-//para recuperar por el id
-app.get('/movies/:id',(req,res)=>{
-    const {id}=req.params 
-    const movie = movies.find(movie => movie.id ==id)
-    if(movie) return res.json(movie)
-      res.status(404).json({mesage: 'Not found'})
-})
-
-app.post('/movies', (req, res) => {    
-   // console.log(req.body)
-    const result = validateMovie(req.body)
-    //console.log(req.body)
-    if(result.error){
-        return res.status(400).json({error: JSON.parse (result.error.mesage)}) //400 bad request
-    }
-    const newMovie ={
-        id:crypto.randomUUID(),
-       ...result.data
-    }
-    //cambiar luego 
-    movies.push(newMovie)
-    res.status(201).json(newMovie) //actualizar la cache del cliente
-})
-
-//actualizar 
-app.patch('/movies/:id', (req, res) => {   
-    const result = validatePartialMovie (req.body)
-    if(result.error){
-        return res.status(400).json({error: JSON.parse (result.error.mesage)}) //400 bad request
-    }
-
-    const {id}= req.params
-    const movieIndex = movies.findIndex(movie=>movie.id==id)
-    if(movieIndex ===-1){
-        return res.status(400).json({mesage: 'Movie not found'})
-    }
-    const updateMovie = {
-    ...movies[movieIndex],
-    ...result.data
-    }
-    movies[movieIndex]=updateMovie
-    return res.json(updateMovie)
-})
-
-/* app.delete('/movies/:id'), (req,res) => {
-    const { id }= req.params 
-    const movieIndex = movies.findIndex(movie => movie.id === id)
-    
-    if(movieIndex === -1){
-        return res.status(404).json({mesage: 'Not found'})
-    } 
-    
-    movies.splice(movieIndex, 1)
-    
-    return res.json({message: 'Movie deleted'})
-}
- */
-app.delete('/movies/:id', (req, res) => {
-    const { id } = req.params
-    const movieIndex = movies.findIndex(movie => movie.id === id)
-  
-    if (movieIndex === -1) {
-      return res.status(404).json({ message: 'Movie not found' })
-    }
-  
-    movies.splice(movieIndex, 1)
-  
-    return res.json({ message: 'Movie deleted' })
-  })
-
-
-/* app.options('/movies/:id'), (req,res)=>{
-  const origin= req.header('origin')
- 
-  if(ACCEPTED_ORIGINS.includes(origin)|| !origin){
-    res.header('Access-Control-Allow-Origin',origin)
-    res.header('Access-Control-Allow-Origin','GET, POST, PUT, PATCH, DELETE')
-  }
-  res.send(200)
-}
- */
+app.use(json())
+app.use(corsMiddleware()) // es un middleware
+app.use('/movies',moviesRouter)
 
 app.listen(1234, ()=>{
     console.log(`server listening on port http://localhost:${1234}`)
--- apiRest/schemas/movie.js
+++ apiRest/schemas/movie.js
@@ -1,4 +1,4 @@
-const z = require('zod') //validaciones tipos de datos
+import z from 'zod' //validaciones tipos de datos
 //validar
 const movieSchema=z.object({
     title:z.string({
@@ -13,15 +13,11 @@ const movieSchema=z.object({
     genre: z.array(z.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']))
    })
 
-function validateMovie(object){
+export function validateMovie(object){
     return movieSchema.safeParse(object)
 }
 
-function validatePartialMovie(object){
+export function validatePartialMovie(object){
     return movieSchema.partial().safeParse(object)
 }
 
-module.exports={
-    validateMovie,
-    validatePartialMovie
-}
